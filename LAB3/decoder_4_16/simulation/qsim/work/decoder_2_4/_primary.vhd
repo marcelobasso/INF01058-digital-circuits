09aa2c695b221f1746030fef2c7e1b3e9e7aa5ec
Marcelo Basso <bassomarcelo.ba@gmail.com>
2024-04-15 01:50:56 -0300

    🔥 deletes useless files

--- LAB3/decoder_4_16/simulation/qsim/work/decoder_2_4/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity decoder_2_4 is
-    port(
-        D               : out    vl_logic_vector(3 downto 0);
-        A               : in     vl_logic_vector(1 downto 0);
-        E               : in     vl_logic
-    );
-end decoder_2_4;
--- LAB3/decoder_4_16/simulation/qsim/work/decoder_4_16/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity decoder_4_16 is
-    port(
-        S               : out    vl_logic_vector(15 downto 0);
-        A               : in     vl_logic_vector(3 downto 0)
-    );
-end decoder_4_16;
